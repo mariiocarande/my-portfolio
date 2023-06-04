b98ef5cf2b8df0ac33d5d07575a1f17a5f723f60
mariiocarande <mdc.mariio@gmail.com>
2023-06-04 18:51:22 -0300

    ADD: navigation between sections

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,29 +1,24 @@
-import Image from 'next/image'
+import { NavItem } from '@/app/page'
 import React from 'react'
 
 interface Props {
-  text: string
+  selectedItem: NavItem
 }
 
-const CardInfo: React.FC<Props> = ({ text }) => (
-  <div className="flex flex-col rounded-md gap-4 bg-gradient-to-r from-slate-300 to-slate-500 p-10 h-full">
-    <h1 className="flex justify-center text-black text-2xl">
-      {text}
-    </h1>
-    <div className="flex justify-center">
-      <Image
-        src="/mario.jpg"
-        width={300}
-        height={300}
-        alt="This is an image of me"
-        className="rounded-full shadow-2xl saturate-200"
-      />
+const CardInfo: React.FC<Props> = ({ selectedItem }) => {
+  return (
+    <div className="rounded-md gap-4 bg-gradient-to-r from-slate-300 to-slate-500 p-10 w-full h-60">
+      <h1 className="flex justify-center text-black text-2xl">
+        {selectedItem.text}
+      </h1>
+      {selectedItem.page === 'about' && (
+        <p className="text-black">
+          {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS. I like learning about new technologies and working with teams that know how to follow good practices.`}
+        </p>
+      )}
     </div>
-    <p className="text-black">
-      {`I'm Mario Carande Scarafía I'm a front-end developer actually working with ReactJS. I like learning about new technologies and working with teams that know how to follow good practices.`}
-    </p>
-  </div>
-)
+  )
+}
 
 export default CardInfo
 
